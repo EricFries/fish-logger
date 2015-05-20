50a89d4bd0047f0bec63cc4d6958f296fbec00b6
EricFries <ericfries@gmail.com>
2015-05-20 14:16:44 -0400

    switch to pg db

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150520172418) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "fish", force: :cascade do |t|
     t.string   "species"
     t.decimal  "weight"
