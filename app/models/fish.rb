cc28f1010b546f596c5bbde9d90eed7178692673
EricFries <ericfries@gmail.com>
2015-05-21 15:24:57 -0400

    setup columsn for photo/map on fish show page

--- app/models/fish.rb
+++ app/models/fish.rb
@@ -3,7 +3,7 @@ class Fish < ActiveRecord::Base
   has_attached_file :image, 
   styles: {
     thumb: '100x100>',
-    medium: '400x300>'
+    medium: '600x450>'
   }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
  
