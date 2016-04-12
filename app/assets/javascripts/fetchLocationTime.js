153a53a7fafc062add889c915375ffcf0a2010c0
Eric Fries <ericfries@Erics-MacBook-Air.local>
2016-04-11 20:32:21 -0400

    move location watch code out of view

--- app/assets/javascripts/fetchLocationTime.js
+++ app/assets/javascripts/fetchLocationTime.js
@@ -1,17 +1,15 @@
 $(function(){
-	$("#reset-location-time").click(function(e){
-		$("#time-log").val("");
-		$("#date-log").val("");
-		$("#latitude-log").val(""); 
+    $("#reset-location-time").click(function(e){
+    	$("#time-log").val("");
+    	$("#date-log").val("");
+    	$("#latitude-log").val(""); 
     $("#longitude-log").val("");
-		getLocation();
-		setDateTime(new Date());
-	});
-
-
+    	getLocation();
+    	setDateTime(new Date());
+    });
 });
 
-  function getLocation() {
+function getLocation() {
     // code here
     navigator.geolocation.getCurrentPosition(
     	function(position){
@@ -32,9 +30,9 @@ function handleLocationError(err){
 	}
 }
 
-	function setDateTime(date){
-		string_date = (date.getMonth() + 1).toString() + "/" + date.getDate().toString() + "/" + date.getFullYear().toString();
-		string_time = date.getHours().toString() + ":" + date.getMinutes().toString();
-		$("#time-log").val(string_time);
-		$("#date-log").val(string_date);
+function setDateTime(date){
+    string_date = (date.getMonth() + 1).toString() + "/" + date.getDate().toString() + "/" + date.getFullYear().toString();
+    string_time = date.getHours().toString() + ":" + date.getMinutes().toString();
+    $("#time-log").val(string_time);
+    $("#date-log").val(string_date);
 }
--- app/assets/javascripts/insertMap.js
+++ app/assets/javascripts/insertMap.js
@@ -1,24 +1,22 @@
 function insertMap(divID, allFish) {
-	
-function initialize() {
-  var mapOptions = {
-    zoom: 12,
-    mapTypeId: google.maps.MapTypeId.ROAD
-  };
-  var map = new google.maps.Map(document.getElementById(divID), mapOptions);
+  function initialize() {
+    var mapOptions = {
+      zoom: 12,
+      mapTypeId: google.maps.MapTypeId.ROAD
+    };
+    var map = new google.maps.Map(document.getElementById(divID), mapOptions);
 
-//Navionics SonarChart
-var navionics_sonarchart_layer = new JNC.Views.gNavionicsOverlay({
-    navKey: "Navionics_webapi_00976",
-    chartType: JNC.Views.gNavionicsOverlay.CHARTS.SONAR,
-    depthUnit: JNC.DEPTH_UNIT.FEET
-});
+  //Navionics SonarChart
+  var navionics_sonarchart_layer = new JNC.Views.gNavionicsOverlay({
+      navKey: "Navionics_webapi_00976",
+      chartType: JNC.Views.gNavionicsOverlay.CHARTS.SONAR,
+      depthUnit: JNC.DEPTH_UNIT.FEET
+  });
 
-map.overlayMapTypes.insertAt(0, navionics_sonarchart_layer);
-dropPins(allFish, map);
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
+  map.overlayMapTypes.insertAt(0, navionics_sonarchart_layer);
+  dropPins(allFish, map);
+  }
+  google.maps.event.addDomListener(window, 'load', initialize);
 }
 
 function dropPins(allFish, map){
@@ -49,5 +47,52 @@ function dropPins(allFish, map){
        bounds.extend(extendPoint2);
     }
   map.fitBounds(bounds);
-  
 }
+
+function initialize() {
+  markerCounter = 0;
+  markerTime = null;
+  infowindow = new google.maps.InfoWindow();
+    function watchLocation() {
+        var mapOptions = {
+        zoom: 12,
+        mapTypeId: google.maps.MapTypeId.ROAD,
+        center: new google.maps.LatLng(41.2939413, -72.3131606)
+        };
+        var map = new google.maps.Map(document.getElementById('watch-map'), mapOptions);
+
+      navigator.geolocation.watchPosition(
+        function(position){
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
+        console.log(latitude); 
+        console.log(longitude);
+        markerCounter ++;
+        markerTime = new Date();
+        dropPin(latitude, longitude, map);
+      }, 
+
+      handleLocationError, 
+
+      {enableHighAccuracy: true, maximumAge: 1000});
+    }
+
+    function dropPin(latitude, longitude, map){ 
+      var myLatlng = new google.maps.LatLng(latitude, longitude);
+      var marker = new google.maps.Marker({
+          position: myLatlng,
+          map: map,
+          animation: google.maps.Animation.DROP,
+          infoContent: '<ul><li>Marker No. ' + markerCounter + '</li><li> ' + markerTime.getHours() + ':' + markerTime.getMinutes() + ':' + markerTime.getSeconds() + '</li>' + '<li>Latitude: ' + latitude + '</li><li>Longitude: ' + longitude
+        });
+
+      google.maps.event.addListener(marker, 'click', (function(marker) { 
+        return function() {
+          infowindow.setContent(marker.infoContent);
+          infowindow.open(map, marker);
+        };
+      })(marker)); 
+    };
+      
+    watchLocation();
+};
